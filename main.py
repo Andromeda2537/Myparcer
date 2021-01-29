db72ce4102f53150b745540d94466d591bbe170a
Andromeda <Xepypg0@yahoo.com>
2021-01-29 19:04:42 +0300

    parser added

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 import requests
 from bs4 import BeautifulSoup
-import time
+from multiprocessing import Pool
 
 
 def get_url(url):
+    """response in html format"""
     response = requests.get(url)
     return response.text
 
 
-def all_links(html):
-    soup = BeautifulSoup(html, 'lmxl')
-    links = soup.find('div', id="bx_1847241719_88")
+def all_links_on_models(html):
+    """links to all models"""
+    soup = BeautifulSoup(html, 'lxml')
+    links = soup.find('div', class_="js_wrapper_items")
+    return links
+
+
+def find_all(html):
+    soup = BeautifulSoup(html, 'lxml')
+    linkss = soup.find('a', class_="thumb shine").get('href')
+    return linkss
+
+
+print(find_all(get_url('https://gastronomia.by/catalog/svezhaya_vypechka/khleb')))
