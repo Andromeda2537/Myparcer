6f2d5ba95abc04b3b23cbb5396c3784971a8ad88
Andromeda <Xepypg0@yahoo.com>
2021-02-10 14:52:45 +0300

    The function of filtering the necessary links to the universal one has been changed. Added functions for creating directory and directory names

---
+++ creating_directory.py
@@ -0,0 +1,17 @@
+import os
+
+
+def name_for_directory(link):
+    """Returns the last element for the directory name
+    from the full link passed as an argument"""
+    directory_name = link.splin('/')[-2]
+    return directory_name
+
+
+def creating_directories(directory_name):
+    """Accepts as an argument the result of the 'name_for_directory' function.
+     If there is no directory with the name received from the function,
+     it creates a directory"""
+    if not os.path.isdir(directory_name):
+        os.makedirs(directory_name)
+    return directory_name
--- main.py
+++ main.py
@@ -1,36 +1,26 @@
 import requests
 from bs4 import BeautifulSoup
-import os
-# from multiprocessing import Pool
 
 
 def get_url(url):
-    """response in html format"""
+    """takes url as an argument, returns the response in html format"""
     response = requests.get(url)
     return response.text
 
 
-def find_all_links(html):
-    """links to all products"""
+def catalog(html, first_tag, second_tag, what_to_take, address, class_="menu-item"):
+    """gets an html document as an argument,
+     returns all links to products in the catalog"""
     soup = BeautifulSoup(html, 'lxml')
-    links = soup.find_all('a', class_="thumb shine")
-    all_scr = []
-    for link in links:
-        scr = link.find('img').get('src')
-        a = 'https://gastronomia.by' + scr
-        all_scr.append(a)
-    return all_scr
+    store_catalog = soup.find_all(first_tag, class_)
+    all_product_links = []
+    for every_element in store_catalog:
+        href = every_element.find(second_tag).get(what_to_take)
+        full_product_link = address + href
+        all_product_links.append(full_product_link)
+    return all_product_links
 
 
-def save_pictures(all_scr):
-    images_path = 'images'
-    if not os.path.exists(images_path):
-        os.makedirs(images_path)
-    with open(images_path + "/gastronomia.jpg", 'ab+') as f:
-        for i in all_scr:
-            jpg = requests.get(i).content
-            f.write(jpg)
-    return
-
-
-print(save_pictures(find_all_links(get_url('https://gastronomia.by/catalog/svezhaya_vypechka/khleb'))))
+html_text = get_url('https://gastronomia.by')
+result = catalog(html_text, 'li', 'a', 'href', 'https://gastronomia.by')
+print(result)
